7ba3b0af9f98af11ae065e779286336cc9e71d0c
Cotory <me@cotory.com>
2024-01-08 21:33:59 +0800

    删除不需要的文件

--- core/Infrastructure/tf-fargate/ecr.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ecr_repository" "ecr" {
-  name = "${var.project}-ecr-${var.env}"
-  
-  tags = {
-    Project     = "${var.project}"
-    Environment = "${var.env}"
-  }
-}
--- core/Infrastructure/tf-fargate/output.tf
+++
@@ -1,3 +0,0 @@
-output "ecr-repository-url" {
-  value = "${aws_ecr_repository.ecr.repository_url}"
-}
